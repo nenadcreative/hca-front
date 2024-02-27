39ad2ab32d3795ef22a56acd031b0c6ed98fd4ad
NenadVrtue <83578623+NenadVrtue@users.noreply.github.com>
2024-02-27 14:32:28 +0100

    typography and colors

---
+++ theme-default.js
@@ -0,0 +1,43 @@
+// Example preset
+module.exports = {
+  theme: {
+    colors: {
+      brand: {
+        blue: "#00A5E4",
+        blue2: "#0395CE",
+        yellow: "#FDC72F",
+        yellowDark: "#E4B32A",
+      },
+      accent: {
+        green: "#91B490",
+      },
+      dark: {
+        dark1: "#151E2D",
+        dark2: "#2C3542",
+        dark3: "#3C4450",
+      },
+      gray: {
+        DEFAULT: "#F5FBFE",
+      },
+    },
+    fontSize: {
+      "text-xs": ["0.875rem", "1.5"],
+      "text-sm": ["1rem", "1.5"],
+      "text-md": ["1.125rem", "1.5"],
+      "text-lg": ["1.25rem", "1.4"],
+      "display-xs": ["1rem", "1.5"],
+      "display-sm": ["1.25rem", "1.25"],
+      "display-md": ["1.5rem", "1.3"],
+      "display-lg": ["2rem", "1.3"],
+      "display-xl": ["3rem", "1.2"],
+      "display-2xl": ["3.75rem", "1.3"],
+      "display-3xl": ["4rem", "1.2"],
+      "display-4xl": ["5rem", "1.2"],
+    },
+    fontFamily: {
+      display: ["Graphik", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
+    },
+    extend: {},
+  },
+};
