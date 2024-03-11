57181fc376046e3143ea65dd424486c8967dde0a
NenadVrtue <83578623+NenadVrtue@users.noreply.github.com>
2024-03-11 09:52:00 +0100

    output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,7 @@ export default defineConfig({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     applyBaseStyles: false
-  }), react()]
+  }), react()],
+  output: "server",
+  adapter: vercel()
 });
