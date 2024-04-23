d3ea64bcd626a9de379695916ef6dffe0b4868a9
Keka321 <123451611+Keka321@users.noreply.github.com>
2024-04-23 08:40:40 +0200

    News and Events

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,28 +1,36 @@
+/** @format */
+
 export function formatDate(date: string) {
-    return new Date(date).toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    });
-  }
-  export function getMonthFromDate(date: string) {
-    return new Date(date).toLocaleDateString("en-US", {
-      month: "long",
-    });
-  }
-  
-  export function formatDateFrom(date: string) {
-    return new Date(date).toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    });
-  }
-  
-  export function formatDateTo(date: string) {
-    const parsedDate = new Date(date);
-  
-    return `${parsedDate.toLocaleDateString("en-US", {
-      day: "numeric",
-    })}, ${parsedDate.getFullYear()}`;
-  }
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
+export function getMonthFromDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+  });
+}
+
+export function formatDateFrom(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+}
+export function formatStartDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
+}
+
+export function formatDateTo(date: string) {
+  const parsedDate = new Date(date);
+
+  return `${parsedDate.toLocaleDateString("en-US", {
+    day: "numeric",
+  })}, ${parsedDate.getFullYear()}`;
+}
