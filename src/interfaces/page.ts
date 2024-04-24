a69a7bdd1f053d8ee8e08b29da87ad0099036ec7
Keka321 <123451611+Keka321@users.noreply.github.com>
2024-04-24 13:40:42 +0200

    Blog

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,11 +1,18 @@
+/** @format */
+
 export default interface Page {
   id: number;
- 
+
   attributes: {
     title: string;
     slug: string;
     SEO: object;
     sections: object;
-    category: string
+    category: string;
+    attributes: object;
+    Hero: any;
+    publishedAt: string;
+    featuredImage: any;
+    content: any;
   };
 }
