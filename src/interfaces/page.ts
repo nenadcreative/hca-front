1fd21ef1f24747fed17e8e1b59c2db2744926006
Keka321 <123451611+Keka321@users.noreply.github.com>
2024-04-24 13:52:08 +0200

    Blog (#61)
    * Blog
    
    * removed blog img
    
    * removing news and events from blog branch
    
    ---------
    
    Co-authored-by: NenadVrtue <83578623+NenadVrtue@users.noreply.github.com>

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,13 +1,19 @@
+/** @format */
+
 export default interface Page {
   id: number;
- 
+
   attributes: {
     title: string;
     slug: string;
     SEO: object;
     sections: object;
-    category: string
+    category: string;
     attributes: object;
     Hero: any;
+    publishedAt: string;
+    featuredImage: any;
+    content: any;
+
   };
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   presets: [require("./theme-default.js")],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
